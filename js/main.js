15dba040affde808d8c99bc26112572e3ddc8c3e
oliviaarden <mac99@SRMAC02.local>
2016-04-25 00:24:11 -0400

    trials and errors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-$('.header').css('rotate');
+var $earth = $('.earth');
+var $grass = $('.grass');
+
+$earth.on('mouseover', function () {
+  $earth.addClass('earth-grow');
+});
+
+$earth.on('mouseout', function () {
+  $earth.removeClass('earth-grow');
+});
+
+$grass.waypoint(function (direction) {
+  if (direction == 'down') {
+  $grass.addClass('grass-fade');
+  } else {
+  $grass.removeClass('grass-fade');
+}
+}, { offset: '100%' });
