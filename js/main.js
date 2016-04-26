e0eae9fb30c8c0567cf5a54ad05ac1f8ea000e7f
oliviaarden <mac99@SRMAC02.local>
2016-04-26 17:30:17 -0400

    adding some things in js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var $earth = $('.earth');
 var $grass = $('.grass');
+var $animals = $('.animals');
 
 $earth.on('mouseover', function () {
   $earth.addClass('earth-grow');
@@ -16,3 +17,11 @@ $grass.waypoint(function (direction) {
     $grass.removeClass('grass-fade');
   }
 }, {offset: '100%'});
+
+$animals.on('mouseover', function () {
+  $animals.addClass('animals-grow');
+});
+
+$animals.on('mouseout', function () {
+  $animals.removeClass('animals-grow');
+});
